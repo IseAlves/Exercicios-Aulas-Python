ebdfe78d683162453495b665448dcff4c9f5219b
Deise S. Alves <98955674+IseAlves@users.noreply.github.com>
2024-06-25 02:13:32 -0300

    Create funcao-range.py

---
+++ funcao-range.py
@@ -0,0 +1,7 @@
+#FUNÇÃO RANGE()
+
+#A função range() é útil para iterarmos uma sequência de números.
+# (o uso de '#' é uma forma de comentarmos em Python)
+for i in range(4):
+    print(i)
+# Apresente o resultado: 0, 1, 2, 3
